c5b22ee5702c77883b35770c934e8dee7df99b54
unknown <katharaon@gmail.com>
2024-02-16 11:23:09 +0530

    pages and router

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
+import Home from './pages/Home'
+import Signup from './pages/Signup'
+import Signin from './pages/Signin'
+import Dashboard from './pages/Dashboard'
+
 
 function App() {
 
   return (
     <>
-    <h1 className='text-5xl text-green-500'>hai</h1>
+    <Routes>
+      <Route path='/' element={<Home/>}></Route>
+      <Route path='/signup' element={<Signup/>}></Route>
+      <Route path='/signin' element={<Signin/>}></Route>
+      <Route path='/dashboard' element={<Dashboard/>}></Route>
+    </Routes>
     </>
   )
 }
